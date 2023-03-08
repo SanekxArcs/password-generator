50797e19b39bdd66cf970b395adead0664038b9d
Oleksandr Dzisiak <shumahersasha007@gmail.com>
2023-03-08 23:14:38 +0100

    update work and styles add dark theme

--- src/components/GeneratePassword/GeneratePassword.jsx
+++ src/components/GeneratePassword/GeneratePassword.jsx
@@ -25,80 +25,101 @@ function GeneratePass() {
     setPasswordLength(e.target.value);
   };
 
+  const handlePasswordResetAll = () => {
+    setPassword("");
+    handleClearPasswordsList();
+  };
+
   return (
-    <div className="p-4 mx-auto w-80">
-      <h1 className="mb-4 text-3xl font-bold">Password Generator</h1>
-      <div className="flex flex-col items-center justify-center p-5 mb-4 rounded-md bg-slate-800">
-        <label className="mr-2">Password Strength:</label>
-        <input
-          type="range"
-          min="8"
-          max="16"
-          value={passwordLength}
-          onChange={handlePasswordLengthChange}
-          className="w-1/2 text-slate-800"
-        />
-        <span className="ml-2">{passwordLength}</span>
-      </div>
-      <div className="flex flex-col gap-3 mb-4">
-        <button
-          onClick={handleGeneratePassword}
-          className="w-full px-4 py-2 mr-2 text-white bg-blue-500 rounded-md"
-        >
-          Generate Password
-        </button>
-        <button
-          onClick={handleCopyToClipboard}
-          disabled={!password}
-          className={`${
-            password ? "bg-green-500" : "bg-gray-300"
-          } text-white px-4 py-2 rounded-md mr-2 w-full`}
-        >
-          Copy Password
-        </button>
-        <button
-          onClick={() => setPassword("")}
-          disabled={!password}
-          className={`${
-            password ? "bg-red-500" : "bg-gray-300"
-          } text-white px-4 py-2 rounded-md mr-2 w-full`}
-        >
-          Reset Generated Password
-        </button>
-      </div>
-      <div className="mb-4">
-        {password && (
-          <div className="p-4 mb-4 border-2 border-gray-500 rounded-md">
-            <p className="mb-2 font-bold">Generated Password:</p>
-            <input
-              type="text"
-              readOnly
-              value={password}
-              className="w-full p-2 border border-gray-400 rounded-md text-slate-800"
-            />
-          </div>
-        )}
-        {passwordsList.length > 0 && (
-          <div className="p-4 border-2 border-gray-500 rounded-md">
-            <p className="mb-2 font-bold">Previous Passwords:</p>
-            {passwordsList.map((pw, i) => (
-              <div key={i} className="mb-2">
-                <input
-                  type="text"
-                  readOnly
-                  value={pw}
-                  className="w-full p-2 border border-gray-400 rounded-md text-slate-800"
-                />
-              </div>
-            ))}
-            <button
-              onClick={handleClearPasswordsList}
-              className="w-full px-4 py-2 mt-4 text-white bg-red-500 rounded-md"
-            >
-              Clear Passwords List
-            </button>
-          </div>
-        )}
+    <div className="mx-auto transition-all duration-500 ">
+      <h1 className="w-full mb-10 text-3xl font-bold text-center sm:text-5xl md:text-6xl lg:text-7xl xl:text-8xl 2xl:text-9xl text-slate-800 dark:text-slate-200">
+        Password Generator
+      </h1>
+      <div className="container mx-auto w-80">
+        <div className="flex flex-col items-center justify-center p-5 mb-4 rounded-md bg-slate-800">
+          <label className="mb-5 text-3xl font-medium text-slate-200 dark:text-slate-200">
+            Strength:<span className="ml-2">{passwordLength}</span>
+          </label>
+          <input
+            type="range"
+            min="8"
+            max="16"
+            value={passwordLength}
+            onChange={handlePasswordLengthChange}
+            className="w-full text-slate-800"
+          />
+
+          <span className="opacity-50">from 8 to 16 symbols</span>
+        </div>
+
+        <div className="flex flex-col items-center justify-center gap-3 mb-4">
+          <button
+            onClick={handleGeneratePassword}
+            className="w-full px-4 py-2 text-xl font-medium uppercase transition-all duration-300 rounded-md shadow text-slate-100 bg-emerald-700 hover:bg-slate-200 hover:text-emerald-700 hover:-translate-y-1"
+          >
+            Generate Password
+          </button>
+
+          <button
+            onClick={handlePasswordResetAll}
+            disabled={!password}
+            className={`${
+              password ? "bg-red-500" : "bg-gray-300"
+            } px-4 py-2 shadow font-medium text-xl uppercase text-slate-100 bg-slate-700 rounded-md w-full hover:bg-slate-200 hover:text-slate-700 hover:-translate-y-1 transition-all duration-300`}
+          >
+            Reset all
+          </button>
+        </div>
+
+        <div className="mb-4">
+          {password && (
+            <div className="w-full p-4 mb-4 rounded-md bg-slate-800">
+              <p className="mb-2 text-lg font-bold text-center uppercase text-slate-200 dark:text-slate-200">
+                Generated Password:
+              </p>
+              <input
+                type="text"
+                readOnly
+                value={password}
+                className="w-full px-2 py-1 font-bold text-center transition-all duration-300 border rounded-md bg-slate-300 hover:bg-slate-50 text-slate-800"
+              />
+              <button
+                onClick={handleCopyToClipboard}
+                disabled={!password}
+                className={`${
+                  password
+                    ? "bg-yellow-500 hover:bg-slate-200 opacity-100"
+                    : "bg-slate-200 hover:text-yellow-500  opacity-10"
+                } px-4 py-2  mt-2 text-xl font-medium uppercase transition-all duration-300 rounded-md shadow text-slate-100 w-full hover:text-yellow-700 hover:-translate-y-1`}
+              >
+                Copy Password
+              </button>
+            </div>
+          )}
+          {passwordsList.length > 1 && (
+            <div className="w-full p-4 transition-all duration-300 rounded-md bg-slate-800">
+              <p className="mb-2 text-lg font-bold text-center uppercase text-slate-200 dark:text-slate-200">
+                Previous Passwords:
+              </p>
+              {passwordsList.map((pw, i) => (
+                <div key={i} className="mb-2">
+                  <input
+                    type="text"
+                    readOnly
+                    value={pw}
+                    className="w-full px-2 py-1 font-bold text-center transition-all duration-300 rounded-md bg-slate-300 text-slate-800 hover:bg-slate-50"
+                  />
+                </div>
+              ))}
+              <button
+                onClick={handleClearPasswordsList}
+                className="w-full px-4 py-2 text-xl font-medium uppercase transition-all duration-300 bg-red-700 rounded-md shadow text-slate-100 hover:bg-slate-200 hover:text-red-700 hover:-translate-y-1"
+              >
+                Clear List
+              </button>
+            </div>
+          )}
+        </div>
       </div>
     </div>
   );
--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -2,7 +2,7 @@ import GeneratePass from "../GeneratePassword/GeneratePassword";
 
 function App() {
   return (
-    <div className="grid py-20 bg-gray-100 dark:bg-slate-900 dark:text-slate-100 text-slate-900 place-items-center min-h-screen h-full">
+    <div className="grid h-full min-h-screen py-20 transition-all duration-300 bg-gray-100 dark:bg-slate-900 dark:text-slate-100 text-slate-900 place-items-center">
       <div className="container mx-auto ">
         <GeneratePass />
       </div>
